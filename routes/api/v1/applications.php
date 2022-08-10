5800dfbfa401ac62cba604e60aea231f566c9fdc
saranyarajamony <45803986+saranyarajamony@users.noreply.github.com>
2022-08-11 09:41:46 +1000

    Update applications.php

--- routes/api/v1/applications.php
+++ routes/api/v1/applications.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+/**
+* Not implemented authentication - considering its not part of the requirement
+*/
 Route::name('emails.')
 ->namespace("\App\Http\Controllers")
 ->group(function () {
-    Route::get('/emails', [\App\Http\Controllers\EmailsController::class, 'index'])
-    ->name('index');
-    Route::post('/emails', [\App\Http\Controllers\EmailsController::class, 'store'])
-    ->name('store');
+    Route::get('/emails', [\App\Http\Controllers\EmailsController::class, 'index'])->name('index'); 
+    Route::post('/emails', [\App\Http\Controllers\EmailsController::class, 'store'])->name('store');
 
 });
